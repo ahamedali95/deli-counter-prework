a6c71e51f9af61cfb9ff6b280aa8c3483fe37153
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-16 21:28:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,13 @@
 
 def line(katz_deli_line)
   current_line = ""
-  
+
   if katz_deli_line.length == 0
     puts "The line is currently empty."
-  else 
+  else
     #"The line is currently: 1. Logan 2. Avi 3. Spencer"
     katz_deli_line.each do |customer, idx|
-      current_line += " #{idx + 1}. #{{customer}}"
+      current_line += " #{idx + 1}. #{customer}"
     end
   end
 
